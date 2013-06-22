fixed sign error in shear matrix in makeGalVMap2 - now pixel and galsim convolution options agree quite well in the fiber velocities they produce (agree to better than 2km/s in the one gal tested, before it was signficantly worse)
git-svn-id: svn+ssh://riemann.lbl.gov/home/mgeorge/svn/speclens@66 71aa9303-448b-4a3d-81da-bc994458fe86

sim.py
@@ -491,7 +491,7 @@ def makeGalVMap2(bulge_n,bulge_r,disk_n,disk_r,bulge_frac,gal_q,gal_beta,gal_flu
 
     # Apply lensing shear to galaxy and velocity maps
     if((g1 != 0.) | (g2 != 0.)):
-        shear=np.array([[1+g1,-g2],[-g2,1-g1]])/np.sqrt(1.-g1**2-g2**2)
+        shear=np.array([[1-g1,-g2],[-g2,1+g1]])/np.sqrt(1.-g1**2-g2**2)
         xs=shear[0,0]*(xx-xCen) + shear[0,1]*(yy-yCen) + xCen
         ys=shear[1,0]*(xx-xCen) + shear[1,1]*(yy-yCen) + yCen
         fluxVMapArr=scipy.ndimage.map_coordinates(fluxVMapArr.T,(xs,ys))