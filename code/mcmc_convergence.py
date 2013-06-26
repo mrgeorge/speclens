4b2fe969b28314f847b862f5445cd2ddc657a388
mgeorge <mgeorge@71aa9303-448b-4a3d-81da-bc994458fe86>
2013-06-26 00:42:25 +0000

    updated makeObs to take args for convolutions to setup observables, and reversed order of ellipse observables and errors after bug fix in sim
    git-svn-id: svn+ssh://riemann.lbl.gov/home/mgeorge/svn/speclens@70 71aa9303-448b-4a3d-81da-bc994458fe86

--- code/mcmc_convergence.py
+++ code/mcmc_convergence.py
@@ -5,19 +5,19 @@ import matplotlib.cm
 import numpy as np
 import sys
 
-def makeObs(nGal,numFib=6,fibRad=1,fibConfig="hexNoCen",sigma=30.,ellErr=np.array([0.1,10])):
+def makeObs(nGal,inputPriors=[[0,360],[0,1],150,(0,0.05),(0,0.05)],disk_r=None,convOpt=None,atmos_fwhm=None,numFib=6,fibRad=1,fibConvolve=False,fibConfig="hexNoCen",sigma=30.,ellErr=np.array([10.,0.1]),seed=None):
 
-    inputPriors=[[0,360],[0,1],150,(0,0.05),(0,0.05)]
-    inputPars=sim.generateEnsemble(nGal,inputPriors,shearOpt=None)
+    inputPars=sim.generateEnsemble(nGal,inputPriors,shearOpt=None,seed=seed)
 
     xvals,yvals=sim.getFiberPos(numFib,fibRad,fibConfig)
-    vvals=np.array([sim.vmapModel(inputPars[ii,:],xvals,yvals) for ii in range(nGal)])
+    if(convOpt is not None):
+        if((type(disk_r) is int) | (type(disk_r) is float)):
+            disk_r=np.repeat(disk_r,nGal)
+        kernel=sim.makeConvolutionKernel(xvals,yvals,atmos_fwhm,fibRad,fibConvolve)
+        vvals=np.array([sim.vmapObs(inputPars[ii,:],xvals,yvals,disk_r[ii],convOpt=convOpt,atmos_fwhm=atmos_fwhm,fibRad=fibRad,fibConvolve=fibConvolve,kernel=kernel) for ii in range(nGal)])
+    else: # this is faster if we don't need to convolve with psf or fiber
+        vvals=np.array([sim.vmapModel(inputPars[ii,:],xvals,yvals) for ii in range(nGal)])
     ellObs=np.array([sim.ellModel(inputPars[ii,:]) for ii in range(nGal)])
-
-    specNoise=np.random.randn(numFib*nGal).reshape((nGal,numFib))*sigma
-    imNoise=np.random.randn(2*nGal).reshape(nGal,2)*ellErr
-    vvals+=specNoise
-    ellObs+=imNoise
 
     return (xvals,yvals,vvals,ellObs,inputPars)
 
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         numFib=6.
         fibRad=1.
         sigma=30.
-        ellErr=np.array([0.1,10])
+        ellErr=np.array([10.,0.1])
         xvals,yvals,vvals,ellObs,inputPars=makeObs(nGal,numFib=numFib,fibRad=fibRad)
         recObs=obsToRec(xvals,yvals,vvals,ellObs)
         labels=np.array(["PA","b/a","vmax","g1","g2"])
@@ -45,7 +45,7 @@ if __name__ == "__main__":
         print "opening old input files"
         fibRad=1.
         sigma=30.
-        ellErr=np.array([0.1,10])
+        ellErr=np.array([10.,0.1])
         recObs=sim.readRec("{}/mcmc_convergence_obs.fits".format(dataDir))
         recPars=sim.readRec("{}/mcmc_convergence_inputPars.fits".format(dataDir))
         xvals,yvals,vvals,ellObs=recToObs(recObs)
