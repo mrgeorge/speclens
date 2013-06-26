bc4ef1ac2edd4e39895a0f622129a3e3a869f7d2
mgeorge <mgeorge@71aa9303-448b-4a3d-81da-bc994458fe86>
2013-06-26 03:37:17 +0000

    rearranged mcmc_convergence into function runEnsemble which will call fitObs and write outputs from ensemble. main will now set up different fiber configureations etc, and eventually will write a qsub job so multiple ensembles can be run at once
    git-svn-id: svn+ssh://riemann.lbl.gov/home/mgeorge/svn/speclens@73 71aa9303-448b-4a3d-81da-bc994458fe86

--- code/mcmc_convergence.py
+++ code/mcmc_convergence.py
@@ -3,7 +3,7 @@ import sim
 import matplotlib.pyplot as plt
 import matplotlib.cm
 import numpy as np
-import sys
+import os
 
 def makeObs(nGal,inputPriors=[[0,360],[0,1],150,(0,0.05),(0,0.05)],disk_r=None,convOpt=None,atmos_fwhm=None,numFib=6,fibRad=1,fibConvolve=False,fibConfig="hexNoCen",sigma=30.,ellErr=np.array([10.,0.1]),seed=None):
 
@@ -21,55 +21,61 @@ def makeObs(nGal,inputPriors=[[0,360],[0,1],150,(0,0.05),(0,0.05)],disk_r=None,c
 
     return (xvals,yvals,vvals,ellObs,inputPars)
 
-
-if __name__ == "__main__":
-
+def runEnsemble(nGal,**kwargs):
     figExt="pdf"
-    plotDir="/data/mgeorge/speclens/plots"
-    dataDir="/data/mgeorge/speclens/data"
-
-    if(sys.argv[1]=="first"):
-        print "creating new input files"
-        nGal=int(sys.argv[2])
-        numFib=6.
-        fibRad=1.
-        sigma=30.
-        ellErr=np.array([10.,0.1])
-        xvals,yvals,vvals,ellObs,inputPars=makeObs(nGal,numFib=numFib,fibRad=fibRad)
-        recObs=obsToRec(xvals,yvals,vvals,ellObs)
-        labels=np.array(["PA","b/a","vmax","g1","g2"])
-        recPars=sim.parsToRec(inputPars,labels=labels)
-        sim.writeRec(recObs,"{}/mcmc_convergence_obs.fits".format(dataDir))
-        sim.writeRec(recPars,"{}/mcmc_convergence_inputPars.fits".format(dataDir))
-    else:
-        print "opening old input files"
-        fibRad=1.
-        sigma=30.
-        ellErr=np.array([10.,0.1])
-        recObs=sim.readRec("{}/mcmc_convergence_obs.fits".format(dataDir))
-        recPars=sim.readRec("{}/mcmc_convergence_inputPars.fits".format(dataDir))
-        xvals,yvals,vvals,ellObs=recToObs(recObs)
-        labels=np.array(["PA","b/a","vmax","g1","g2"])
-        inputPars=sim.recToPars(recPars,labels=labels)
+    plotDir="/data/mgeorge/speclens/plots/"
+    dataDir="/data/mgeorge/speclens/data/"
+    subDir="opt_{}_{}_{}_{}_{}_{}".format(convOpt,atmos_fwhm,numFib,fibRad,fibConvolve,fibConfig)
+    if(not(os.path.exists(dataDir+subDir))):
+        os.makedirs(dataDir+subDir)
+    if(not(os.path.exists(plotDir+subDir))):
+        os.makedirs(plotDir+subDir)
 
     obsPriors=[[0,360],[0,1],(150,15),[-0.5,0.5],[-0.5,0.5]]
-
-    numFib=len(xvals)
-    nGal=len(vvals)
+    labels=np.array(["PA","b/a","vmax","g1","g2"])
     nPars=len(obsPriors)
-    nBurn=np.array([20,50,50,100,100])
-    nSteps=np.array([100,200,300,500,700])
-    nMCMC=len(nBurn)
-
-    obsParsI=np.zeros((nMCMC,nGal,nPars))
+    obsParsI=np.zeros((nGal,nPars))
     obsParsS=np.zeros_like(obsParsI)
     obsParsIS=np.zeros_like(obsParsI)
 
-    atmos_fwhm=1.5
+    xvals,yvals,vvals,ellObs,inputPars=makeObs(nGal,**kwargs)
+    sim.writeRec(sim.parsToRec(inputPars,labels=labels),dataDir+subDir+"/inputPars.fits")
+
+    for ii in range(nGal):
+        print "************Running Galaxy {}".format(ii)
+
+        chains,lnprobs=sim.fitObs(vvals[ii,:],sigma,ellObs[ii,:],ellErr,obsPriors,disk_r=disk_r,convOpt=convOpt,atmos_fwhm=atmos_fwhm,fibRad=fibRad,fibConvolve=fibConvolve,fibConfig=fibConfig,addNoise=True,seed=seed)
+        obsParsI[ii,:]=sim.getMaxProb(chains[0],lnprobs[0])
+        obsParsS[ii,:]=sim.getMaxProb(chains[1],lnprobs[1])
+        obsParsIS[ii,:]=sim.getMaxProb(chains[2],lnprobs[2])
+
+        # write (and re-write) fits file output as we go
+        sim.writeRec(sim.parsToRec(obsParsI[0:ii+1],labels=labels),dataDir+subDir+"/obsParsI.fits")
+        sim.writeRec(sim.parsToRec(obsParsS[0:ii+1],labels=labels),dataDir+subDir+"/obsParsS.fits")
+        sim.writeRec(sim.parsToRec(obsParsIS[0:ii+1],labels=labels),dataDir+subDir+"/obsParsIS.fits")
+
+        sim.contourPlotAll(chains,inputPars=inputPars[ii],smooth=3,percentiles=[0.68,0.95],labels=labels,showPlot=False,filename=plotDir+subDir+"/gal_{}.{}".format(ii,figExt))
+
+
+
+if __name__ == "__main__":
+
+
+    nGal=100
     disk_r=1.
-    fibConvolve=True
-    convOpt="pixel"
+    convOpt=np.array([None,"pixel"])
+    atmos_fwhm=np.array([None,1.5])
+    numFib=np.array([6,6])
+    fibRad=np.array([1.,1.])
+    fibConvolve=np.array([False,True])
+    fibConfig=np.array(["hexNoCen","hexNoCen"])
+    seed=7
     
+    runEnsemble(nGal,disk_r=disk_r,convOpt=convOpt,atmos_fwhm=atmos_fwhm,numFib=numFib,fibRad=fibRad,fibConvolve=fibConvolve,fibConfig=fibConfig,seed=seed)
+    
+
+
+
     for mm in range(nMCMC):
         print "************MCMC {}".format(mm)
         for ii in range(nGal):
