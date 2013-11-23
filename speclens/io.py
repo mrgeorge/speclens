561ed21597a2b079d70a2f745efd7d6ab5e23c8c
Matt George <mattgeorge1@gmail.com>
2013-11-22 19:07:05 -0800

    brief docstrings for each function in io.py, fixes #3

--- speclens/io.py
+++ speclens/io.py
@@ -4,13 +4,16 @@ import fitsio
 import numpy as np
 
 def writeRec(rec,filename,clobber=True,compress="GZIP"):
+    """Write recarray to fits file"""
     fitsio.write(filename,rec,clobber=clobber,compress=compress)
 
 def readRec(filename):
+    """Read fits file into recarray"""
     rec=fitsio.read(filename)
     return rec
 
 def parsToRec(pars,labels=np.array(["PA","b/a","vmax","g1","g2"])):
+    """Convert pars ndarray to recarray"""
     dtype=[(label,float) for label in labels]
     rec=np.recarray(len(pars),dtype=dtype)
     for ii in range(len(labels)):
@@ -18,6 +21,7 @@ def parsToRec(pars,labels=np.array(["PA","b/a","vmax","g1","g2"])):
     return rec
 
 def chainToRec(chain,lnprob,labels=np.array(["PA","b/a","vmax","g1","g2"])):
+    """Convert chain and lnprob to recarray"""
     nGal=chain.shape[0]
     nPars=chain.shape[1]
     arr=np.zeros((nGal,nPars+1))
@@ -28,6 +32,7 @@ def chainToRec(chain,lnprob,labels=np.array(["PA","b/a","vmax","g1","g2"])):
     return rec
 
 def recToPars(rec,labels=np.array(["PA","b/a","vmax","g1","g2"])):
+    """Convert recarray to pars array"""
     recLabels=rec.dtype.fields.keys() # note, this list is unordered since rec is a dict, so we need to use parsLabels (which should be sorted to match the order of columns in pars array)
     pars=np.zeros((len(rec),len(labels)))
     for ii in range(len(labels)):
@@ -35,6 +40,7 @@ def recToPars(rec,labels=np.array(["PA","b/a","vmax","g1","g2"])):
     return pars
 
 def obsToRec(xvals,yvals,vvals,ellObs):
+    """Convert array of observables to recarray"""
     dtype=[("xvals",(xvals.dtype.type,xvals.shape)),("yvals",(yvals.dtype.type,yvals.shape)),("vvals",(vvals.dtype.type,vvals.shape)),("ellObs",(ellObs.dtype.type,ellObs.shape))]
     rec=np.recarray(1,dtype=dtype)
     rec["xvals"]=xvals
@@ -44,6 +50,7 @@ def obsToRec(xvals,yvals,vvals,ellObs):
     return rec
 
 def recToObs(rec):
+    """Convert recarray of ndarrays of observables"""
     xvals=rec["xvals"].squeeze()
     yvals=rec["yvals"].squeeze()
     vvals=rec["vvals"].squeeze()
@@ -51,6 +58,7 @@ def recToObs(rec):
     return (xvals,yvals,vvals,ellObs)
 
 def statsToRec(inputPars,mp,kde,hw):
+    """Store input pars array and some chain stats as recarray"""
     dtype=[("inputPars",(inputPars.dtype.type,inputPars.shape)),("mp",(mp.dtype.type,mp.shape)),("kde",(kde.dtype.type,kde.shape)),("hw",(hw.dtype.type,hw.shape))]
     rec=np.recarray(1,dtype=dtype)
     rec["inputPars"]=inputPars
