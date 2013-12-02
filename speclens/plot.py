99c62cbc9dd3bcc13140ba1a28090f4099ac155a
Matt George <mattgeorge1@gmail.com>
2013-12-02 15:44:14 -0800

    replaced tabs with 4 spaces in plot.py

--- speclens/plot.py
+++ speclens/plot.py
@@ -24,9 +24,9 @@ def showImage(profile,xfib,yfib,fibRad,fibShape="circle",fibPA=None,filename=Non
     #    img.setCenter(0,0)
 
     if(plotScale=="linear"):
-	plotArr=img.array
+        plotArr=img.array
     elif(plotScale=="log"):
-	plotArr=np.log(img.array)
+        plotArr=np.log(img.array)
 
     plt.imshow(plotArr,origin='lower',extent=(-halfWidth,halfWidth,-halfWidth,halfWidth),interpolation='nearest',cmap=cmap)
 
@@ -49,40 +49,40 @@ def showImage(profile,xfib,yfib,fibRad,fibShape="circle",fibPA=None,filename=Non
                 ax.add_patch(sq)
 
     if(colorbar):
-	cbar=plt.colorbar(fraction=0.05)
-	if(colorbarLabel is not None):
-	    cbar.set_label(colorbarLabel)
+        cbar=plt.colorbar(fraction=0.05)
+        if(colorbarLabel is not None):
+            cbar.set_label(colorbarLabel)
 
     if(ellipse is not None): # ellipse is either None or np.array([disk_r,gal_q,gal_beta])
-	ax=plt.gca()
-	rscale=2
-	ell=matplotlib.patches.Ellipse(xy=(0,0),width=rscale*ellipse[0]*ellipse[1],height=rscale*ellipse[0],angle=ellipse[2]-90,fill=False,color="white",lw=lw)
-	ax.add_artist(ell)
+        ax=plt.gca()
+        rscale=2
+        ell=matplotlib.patches.Ellipse(xy=(0,0),width=rscale*ellipse[0]*ellipse[1],height=rscale*ellipse[0],angle=ellipse[2]-90,fill=False,color="white",lw=lw)
+        ax.add_artist(ell)
     
     if(lines is not None): # lines is either None or np.array([[x1,x2,y1,y2],...]) or np.array([x1,x2,y1,y2])
-	    if(lines.shape == (4,)): # only one line		
-		plt.plot(lines[0:2],lines[2:4],color=lcolors,lw=lw,ls=lstyles)
-            else:
-		if(type(lcolors) is str):
-		    lcolors=np.repeat(lcolors,len(lines))
-		if(type(lstyles) is str):
-		    lstyles=np.repeat(lstyles,len(lines))
-		for line,color,style in zip(lines,lcolors,lstyles):
-		    plt.plot(line[0:2],line[2:4],color=color,lw=lw,ls=style)
-		    
+        if(lines.shape == (4,)): # only one line
+            plt.plot(lines[0:2],lines[2:4],color=lcolors,lw=lw,ls=lstyles)
+        else:
+            if(type(lcolors) is str):
+                lcolors=np.repeat(lcolors,len(lines))
+            if(type(lstyles) is str):
+                lstyles=np.repeat(lstyles,len(lines))
+            for line,color,style in zip(lines,lcolors,lstyles):
+                plt.plot(line[0:2],line[2:4],color=color,lw=lw,ls=style)
+    
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     if(trim>0): # trim all edges by this amount in arcsec
-	plt.xlim((-halfWidth+trim,halfWidth-trim))
-	plt.ylim((-halfWidth+trim,halfWidth-trim))
+        plt.xlim((-halfWidth+trim,halfWidth-trim))
+        plt.ylim((-halfWidth+trim,halfWidth-trim))
 
     if(title is not None):
         plt.title(title)
 
     if(filename):
-	plt.savefig(filename,bbox_inches=matplotlib.transforms.Bbox(np.array(((0,-.4),(8.5,6))))) # annoying hardcode fudge to keep labels inside plot
+        plt.savefig(filename,bbox_inches=matplotlib.transforms.Bbox(np.array(((0,-.4),(8.5,6))))) # annoying hardcode fudge to keep labels inside plot
     if(showPlot):
-	plt.show()
+        plt.show()
 
 
 def showArr(arr):
@@ -101,62 +101,62 @@ def contourPlot(xvals,yvals,smooth=0,percentiles=[0.68,0.95,0.99],colors=["red",
 
     # if it's a single ndarray wrapped in a list, convert to ndarray to use full color list
     if((type(xvals) is list) & (len(xvals) ==1)):
-	xvals=xvals[0]
-	yvals=yvals[0]
-	
+        xvals=xvals[0]
+        yvals=yvals[0]
+
     if(type(xvals) is list):
-	for ii in range(len(xvals)):
-	    zz,xx,yy=np.histogram2d(xvals[ii],yvals[ii],bins=n2dbins)
-	    xxbin=xx[1]-xx[0]
-	    yybin=yy[1]-yy[0]
-	    xx=xx[1:]+0.5*xxbin
-	    yy=yy[1:]+0.5*yybin
+        for ii in range(len(xvals)):
+            zz,xx,yy=np.histogram2d(xvals[ii],yvals[ii],bins=n2dbins)
+            xxbin=xx[1]-xx[0]
+            yybin=yy[1]-yy[0]
+            xx=xx[1:]+0.5*xxbin
+            yy=yy[1:]+0.5*yybin
 
-	    if(smooth > 0):
-		kernSize=int(10*smooth)
-		sx,sy=scipy.mgrid[-kernSize:kernSize+1, -kernSize:kernSize+1]
-		kern=np.exp(-(sx**2 + sy**2)/(2.*smooth**2))
-		zz=scipy.signal.convolve2d(zz,kern/np.sum(kern),mode='same')
-	
-	    hist,bins=np.histogram(zz.flatten(),bins=1000)
-	    sortzz=np.sort(zz.flatten())
-	    cumhist=np.cumsum(sortzz)*1./np.sum(zz)
-	    levels=np.array([sortzz[(cumhist>(1-pct)).nonzero()[0][0]] for pct in percentiles])
+        if(smooth > 0):
+            kernSize=int(10*smooth)
+            sx,sy=scipy.mgrid[-kernSize:kernSize+1, -kernSize:kernSize+1]
+            kern=np.exp(-(sx**2 + sy**2)/(2.*smooth**2))
+            zz=scipy.signal.convolve2d(zz,kern/np.sum(kern),mode='same')
 
-	    plt.contour(xx,yy,zz.T,levels=levels,colors=colors[ii])
+        hist,bins=np.histogram(zz.flatten(),bins=1000)
+        sortzz=np.sort(zz.flatten())
+        cumhist=np.cumsum(sortzz)*1./np.sum(zz)
+        levels=np.array([sortzz[(cumhist>(1-pct)).nonzero()[0][0]] for pct in percentiles])
+
+        plt.contour(xx,yy,zz.T,levels=levels,colors=colors[ii])
     else: #we just have single ndarrays for xvals and yvals
         zz,xx,yy=np.histogram2d(xvals,yvals,bins=n2dbins)
-	xxbin=xx[1]-xx[0]
-	yybin=yy[1]-yy[0]
-	xx=xx[1:]+0.5*xxbin
-	yy=yy[1:]+0.5*yybin
+        xxbin=xx[1]-xx[0]
+        yybin=yy[1]-yy[0]
+        xx=xx[1:]+0.5*xxbin
+        yy=yy[1:]+0.5*yybin
 
-	if(smooth > 0):
-	    kernSize=int(10*smooth)
-	    sx,sy=scipy.mgrid[-kernSize:kernSize+1, -kernSize:kernSize+1]
-	    kern=np.exp(-(sx**2 + sy**2)/(2.*smooth**2))
-	    zz=scipy.signal.convolve2d(zz,kern/np.sum(kern),mode='same')
-	
-	    hist,bins=np.histogram(zz.flatten(),bins=1000)
-	    sortzz=np.sort(zz.flatten())
-	    cumhist=np.cumsum(sortzz)*1./np.sum(zz)
-	    levels=np.array([sortzz[(cumhist>(1-pct)).nonzero()[0][0]] for pct in percentiles])
+        if(smooth > 0):
+            kernSize=int(10*smooth)
+            sx,sy=scipy.mgrid[-kernSize:kernSize+1, -kernSize:kernSize+1]
+            kern=np.exp(-(sx**2 + sy**2)/(2.*smooth**2))
+            zz=scipy.signal.convolve2d(zz,kern/np.sum(kern),mode='same')
 
-	    plt.contour(xx,yy,zz.T,levels=levels,colors=colors)
+        hist,bins=np.histogram(zz.flatten(),bins=1000)
+        sortzz=np.sort(zz.flatten())
+        cumhist=np.cumsum(sortzz)*1./np.sum(zz)
+        levels=np.array([sortzz[(cumhist>(1-pct)).nonzero()[0][0]] for pct in percentiles])
+
+        plt.contour(xx,yy,zz.T,levels=levels,colors=colors)
 
     if(xlabel is not None):
-	plt.xlabel(xlabel)
+        plt.xlabel(xlabel)
     if(ylabel is not None):
-	plt.ylabel(ylabel)
+        plt.ylabel(ylabel)
     if(xlim is not None):
-	plt.xlim(xlim)
+        plt.xlim(xlim)
     if(ylim is not None):
-	plt.ylim(ylim)
+        plt.ylim(ylim)
 
     if(filename):
-	plt.savefig(filename)
+        plt.savefig(filename)
     if(showPlot):
-	plt.show()
+        plt.show()
     
 def contourPlotAll(chains,lnprobs=None,inputPars=None,showMax=True,showPeakKDE=True,show68=True,smooth=0,percentiles=[0.68,0.95,0.99],colors=["red","green","blue"],labels=None,figsize=(8,6),filename=None,showPlot=False):
     """Make a grid of contour plots for each pair of parameters
@@ -171,26 +171,26 @@ def contourPlotAll(chains,lnprobs=None,inputPars=None,showMax=True,showPeakKDE=T
     fig.subplots_adjust(hspace=0,wspace=0)
 
     if(labels is None):
-	labels=np.repeat(None,nPars)
+        labels=np.repeat(None,nPars)
 
     # find max and min for all pars across chains
     limArr=np.tile((np.Inf,-np.Inf),nPars).reshape(nPars,2)
     for ch in chains:
-	for par in range(nPars):
-	    lo,hi=np.min(ch[:,par]), np.max(ch[:,par])
-	    if(lo < limArr[par,0]):
-		limArr[par,0]=lo.copy()
-	    if(hi > limArr[par,1]):
-		limArr[par,1]=hi.copy()
+        for par in range(nPars):
+            lo,hi=np.min(ch[:,par]), np.max(ch[:,par])
+            if(lo < limArr[par,0]):
+                limArr[par,0]=lo.copy()
+            if(hi > limArr[par,1]):
+                limArr[par,1]=hi.copy()
 
     # handle colors
     if(len(colors) == len(chains)):
-	histColors=colors
-	contourColors=colors
+        histColors=colors
+        contourColors=colors
     if((nChains == 1) & (len(colors) == len(percentiles))):
-	histColors=colors[0]
-	contourColors=colors
-	    
+        histColors=colors[0]
+        contourColors=colors
+    
     # Get max posterior and width
     if((showMax) & (lnprobs is not None)):
         maxProbs=np.array([fit.getMaxProb(ch,lnp) for ch,lnp in zip(chains,lnprobs)])
@@ -201,29 +201,29 @@ def contourPlotAll(chains,lnprobs=None,inputPars=None,showMax=True,showPeakKDE=T
         
     # fill plot panels
     for row in range(nPars):
-	for col in range(nPars):
-	    fig.sca(axarr[row,col])
+        for col in range(nPars):
+            fig.sca(axarr[row,col])
 
-	    # setup axis labels
-	    if(row == nPars-1):
-		xlabel=labels[col]
-		plt.setp(axarr[row,col].get_xticklabels(), rotation="vertical", fontsize="xx-small")
+            # setup axis labels
+            if(row == nPars-1):
+                xlabel=labels[col]
+                plt.setp(axarr[row,col].get_xticklabels(), rotation="vertical", fontsize="xx-small")
             else:
-		xlabel=None
-		plt.setp(axarr[row,col].get_xticklabels(),visible=False)
-	    if(col == 0):
-		ylabel=labels[row]
-		plt.setp(axarr[row,col].get_yticklabels(), fontsize="xx-small")
+                xlabel=None
+                plt.setp(axarr[row,col].get_xticklabels(),visible=False)
+            if(col == 0):
+                ylabel=labels[row]
+                plt.setp(axarr[row,col].get_yticklabels(), fontsize="xx-small")
             else:
-		ylabel=None
-		plt.setp(axarr[row,col].get_yticklabels(),visible=False)
-		    
-	    xarrs=[chain[:,col] for chain in chains]
-	    yarrs=[chain[:,row] for chain in chains]
-	    xlim=limArr[col]
-	    ylim=limArr[row]
-	    if(row == col):
-                #		histvals=axarr[row,col].hist(xarrs,bins=50,range=xlim,histtype="step",color=histColors)
+                ylabel=None
+                plt.setp(axarr[row,col].get_yticklabels(),visible=False)
+    
+            xarrs=[chain[:,col] for chain in chains]
+            yarrs=[chain[:,row] for chain in chains]
+            xlim=limArr[col]
+            ylim=limArr[row]
+            if(row == col):
+                #histvals=axarr[row,col].hist(xarrs,bins=50,range=xlim,histtype="step",color=histColors)
                 xKDE=np.linspace(xlim[0],xlim[1],num=50)
                 for ii in range(nChains):
                     kern=scipy.stats.gaussian_kde(xarrs[ii])
@@ -242,26 +242,26 @@ def contourPlotAll(chains,lnprobs=None,inputPars=None,showMax=True,showPeakKDE=T
                     # add vertical lines marking the input value
                     plt.plot(np.repeat(inputPars[col],2),np.array(plt.gca().get_ylim()),color="yellow",ls="--")
 
-		if(xlabel is not None):
-		    axarr[row,col].set_xlabel(xlabel)
-		if(ylabel is not None):
-		    axarr[row,col].set_ylabel(ylabel)
-		axarr[row,col].set_xlim(xlim)
-		plt.setp(axarr[row,col].get_yticklabels(),visible=False)
-	    elif(col < row):
-		contourPlot(xarrs,yarrs,smooth=smooth,percentiles=percentiles,colors=contourColors,xlabel=xlabel,ylabel=ylabel)
-		axarr[row,col].set_xlim(xlim)
-		axarr[row,col].set_ylim(ylim)
+                if(xlabel is not None):
+                    axarr[row,col].set_xlabel(xlabel)
+                if(ylabel is not None):
+                    axarr[row,col].set_ylabel(ylabel)
+                axarr[row,col].set_xlim(xlim)
+                plt.setp(axarr[row,col].get_yticklabels(),visible=False)
+            elif(col < row):
+                contourPlot(xarrs,yarrs,smooth=smooth,percentiles=percentiles,colors=contourColors,xlabel=xlabel,ylabel=ylabel)
+                axarr[row,col].set_xlim(xlim)
+                axarr[row,col].set_ylim(ylim)
                 if(inputPars is not None):
                     # add lines marking the input values
                     plt.plot(np.repeat(inputPars[col],2),ylim,color="yellow",ls="--")
                     plt.plot(xlim,np.repeat(inputPars[row],2),color="yellow",ls="--")
-	    else:
-		axarr[row,col].axis("off")
+            else:
+                axarr[row,col].axis("off")
 
     fig.subplots_adjust(bottom=0.15)
     if(filename):
-	fig.savefig(filename)
+        fig.savefig(filename)
     if(showPlot):
-	fig.show()
+        fig.show()
 
