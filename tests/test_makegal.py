6a93bf649c579f44cae6a4a42340b272ca3198cb
Matt George <mattgeorge1@gmail.com>
2013-12-12 18:12:36 -0800

    started test_image to compare galsim and bessel image profiles
    see also scratch/make3dgal.py

--- tests/test_makegal.py
+++ tests/test_makegal.py
@@ -13,9 +13,37 @@ except ImportError: # add parent dir to python search path
     import speclens
 
 
-def test_image():
-    gal=speclens.Galaxy()
-    obs=speclens.Observation(gal)
+def test_image(): 
+
+    diskRadius=1.
+    diskSersic=1.
+    diskFlux=1.
+    bulgeFraction=0.
+    bulgeRadius=1.
+    bulgeSersic=4.
+
+    redshift=0.5
+    diskPA=0.
+    g1=0.
+    g2=0.
+    atmosFWHM=None
+
+    galBA=1.
+    galCA=0.2
+    diskInclination=speclens.sim.convertInclination(galBA=galBA, 
+                                                    galCA=galCA)
+    
+    gal=speclens.Galaxy(diskRadius=diskRadius, diskSersic=diskSersic,
+                        diskFlux=diskFlux,
+                        bulgeFraction=bulgeFraction,
+                        bulgeRadius=bulgeRadius,
+                        bulgeSersic=bulgeSersic)
+    obs=speclens.Observation(gal, redshift=redshift, diskPA=diskPA.,
+                             diskInclination=diskInclination, g1=0., 
+                             g2=0., atmosFWHM=None) 
     im1=obs.getImage()
 
-    im2=
+    im2=speclens.sim.makeImageBessel(bulgeSersic, bulgeRadius,
+                                     diskSersic, diskRadius,
+                                     bulgeFraction, galBA, diskPA,
+                                     galCA)
