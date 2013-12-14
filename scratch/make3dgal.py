5bad8ebf0ee15645a4b090582d0e6e18ff97d689
Matt George <mattgeorge1@gmail.com>
2013-12-13 17:48:58 -0800

    tests with make3dgal help find effective r_s for inclined thick disks
    producing 3d flux densities with Bessel functions following Spergel
    2010 and producing projected images at different inclinations, results
    suggest that effective scale radius can reasonably be approximated as
    ry' = ry * sqrt[1 - sin(inc)**2 * (1 - (rz/ry)**2)] where ry=rx is the
    scale radius for the face-on disk and rz is the disk thickness. The
    approximation doesn't seem as good for face-on thin disks (Bessel
    profiles appear less cuspy than exponential) and it hasn't been tested
    for different values of nu, which approximate different Sersic
    indices.
    
    Continuing work on #8

--- scratch/make3dgal.py
+++ scratch/make3dgal.py
@@ -14,8 +14,23 @@ def ndmesh(*args):
 
 def fnu(nu,u):
     """Spergel 2010 Eq 4"""
-    return (u/2.)**nu * scipy.special.kv(nu,u) / scipy.special.gamma(nu+1.)
+    ff = (u/2.)**nu * scipy.special.kv(nu,u) / scipy.special.gamma(nu+1.)
 
+    # handle the u=0 case (at least for nu>0)
+    # Note error in Spergel 2010 following Eq. 7,
+    #   small u behavior is f->1/(2nu), not 1/(2(nu+1))
+    #   (check examples in Eqs 6 & 7 to verify)
+    # First try to catch u==0 assuming u is an array
+    #   then handle the scalar case if exception is thrown
+    try:
+        if(nu > 0):
+            ff[u==0] = 1./(2.*nu)
+    except IndexError:
+        if(nu > 0):
+            if(u==0):
+                ff = 1./(2.*nu)
+    return ff
+            
 def bessel3d(nu, arg):
     const=1.
     return const*fnu(nu-0.5,arg)
@@ -35,27 +50,36 @@ def exp3d(arg):
     
 if __name__ == "__main__":
 
+    # define x,y,z grid
     xx,yy,zz=ndmesh(range(100),range(100),range(100))
+    # centroid positions aren't integer 50 because
+    #   bessel3d diverges at r=0 for nu<=0.5
+    xcen,ycen,zcen=49.9,49.9,49.9
 
-    xcen,ycen,zcen=49.5,49.5,49.5
-    rx,ry,rz=10.,10.,10.
+    # define scale lengths (in pixels)
+    # play with rz to change thickness
+    rx,ry,rz=10.,10.,0.1
 
-
+    # play with inclination for tilt (rotation about x-axis)
     inc=np.deg2rad(0.)
+
+    # define inclined coordinates
     xp=xx
     yp=(yy-ycen)*np.cos(inc) - (zz-zcen)*np.sin(inc) + ycen
     zp=(yy-ycen)*np.sin(inc) + (zz-zcen)*np.cos(inc) + zcen
 
+    # 3d radius scaled by each dimension's scale length
     arg=r3d(xx,yy,zz,xcen,ycen,zcen,rx,ry,rz)
     
-    ff=exp3d(arg)
-    fp=scipy.ndimage.map_coordinates(ff.T,[xp,yp,zp])
-
-    nu=0.5
-    cnu=1.67835
+    # evaluate 3d luminosity density with bessel functions
+    nu=0.5 # 0.5 for exponential, -0.6 for ~deVaucouleur's
+    cnu=1.67835 # convert half-light to exponential radius
     bb=bessel3d(nu,arg)
+
+    # evaluate luminosity density at inclined coordinates
     bp=scipy.ndimage.map_coordinates(bb.T,[xp,yp,zp])
 
+    # compare original and inclined images in each projected dimension
     plt.imshow(bb.sum(axis=0),interpolation="nearest") # xy
     plt.show()
     plt.imshow(bp.sum(axis=0),interpolation="nearest")
@@ -70,8 +94,14 @@ if __name__ == "__main__":
     plt.show()
 
     # vertical slice in xy plane
+    # this tests what the effective scale radius is in the 
+    #    inclined (y) dimension. To a good approximation,
+    #    ry' = ry * sqrt[1 - sin(inc)**2 * (1-(rz/ry)**2)]
+    #    which gives ry * cos(inc) as rz->0 (thin disk)
+    #    and ry or rz as rz->ry (spherical case)
+    # But face on bessel disks appear less cuspy then exp
     yarr=np.arange(100)
-    plt.plot(bp.sum(axis=0)[:,50]/np.max(bp.sum(axis=0)[:,50]))
-    plt.plot(np.exp(-np.sqrt(((yarr-ycen)/(ry*np.cos(inc)))**2)))
+    plt.plot(bp.sum(axis=0)[:,50]/np.max(bp.sum(axis=0)[:,50]),'blue')
+    plt.plot(bp.sum(axis=0)[:,50],'red')
+    plt.plot(np.exp(-np.sqrt(((yarr-ycen)/(ry*np.sqrt(1.-np.sin(inc)**2*(1.-(rz/ry)**2))))**2)),'green')
     plt.show()
-    
