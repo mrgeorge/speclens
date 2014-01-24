3aa4152054ed461c269697a2219ac05975c25b98
emhuff <emhuff@gmail.com>
2014-01-24 18:02:23 -0500

    datacube class runs, generates datacubes with some resemblance to correct.

--- scratch/datacube.py
+++ scratch/datacube.py
@@ -16,8 +16,8 @@ except ImportError: # add parent dir to python search path
 import speclens.sim as sim
 
 
-class EmissionLine(object,center = 3727.30, width = 1., peak = 1e-17, name="OII"):
-    def __init__(self):
+class EmissionLine(object):
+    def __init__(self,center = 3727.30, width = 1., peak = 1e-17, name="OII"):
         self.Center = center
         self.Width = width #Linewidth (angstroms)
         self.PeakFlux = peak
@@ -34,7 +34,7 @@ class Datacube(object):
     but don't actually build until needed.
     """
 
-    def __init__(self, model, linelist):
+    def __init__(self, model = None, linelist = None):
         if model == None:
             model = speclens.Model("B")
         assert isinstance(model,speclens.Model), "Argument to Datacube, if provided, must be a Model class instance"
@@ -48,24 +48,24 @@ class Datacube(object):
         self.seeingScale = 40. # characteristic wavenumber of seeing disk, in arcsec^-1. 40 gets us a ~1'' psf
         self.pixelScale = 0.2  # Image Pixel scale, in arcsec per pixel
         self.deltaLambdaObs = 1. # wavelength scale, in Angstroms per pixel
-        self.LambaObsMin = 3000. # Starting wavelength, in Angstroms
+        self.LambdaObsMin = 3000. # Starting wavelength, in Angstroms
         self.nSpectralPixels = 1000.
         self.lambdaObs = self.deltaLambdaObs * np.arange(self.nSpectralPixels)+ self.LambdaObsMin
         
         #Parameters describing the galaxy spectrum.
-        self.baseModel = Model
-        self.z = z # galaxy redshift
+        self.baseModel = model
+        self.z = model.redshift # galaxy redshift
         self.ContinuumReferenceWavelength = 5000. # Wavelength at which to normalize the continuum, in Angstroms
         self.ContinuumReferenceNorm = 1e-18 # Value of continuum at reference wavelength, in erg/s/cm^2
         
         #Set up the default linelist. The user can change this.
         if linelist == None:
             self._defaultLinelist = ["OII","Hbeta","OIIIa","OIIIb","Halpha"] # Name of emission line.
-            self._detaultLineCenters = [3727.30, 4861.33, 4958.91, 5006.84, 6562.82] # Location of line center, in Angstroms
+            self._defaultLineCenters = [3727.30, 4861.33, 4958.91, 5006.84, 6562.82] # Location of line center, in Angstroms
             self._defaultLinewidth =  [1.,1.,1.,1.,1.] # 1-sigma width of emission line, in Angstroms
             self._defaultLinePeakFlux = [1e-17, 1e-17, 1e-17, 1e-17, 1e-17] # peak line flux, in erg/s/cm^2
             self.LineList = []
-            for i in range(len(self._defaultLinePeakFux)): 
+            for i in range(len(self._defaultLinePeakFlux)): 
                 self.LineList.append(EmissionLine(center = self._defaultLineCenters[i],\
                                                   width  = self._defaultLinewidth[i],\
                                                   peak = self._defaultLinePeakFlux[i],\
@@ -80,7 +80,7 @@ class Datacube(object):
         image = sim.makeImageBessel(self.galaxyModel)
         #Make a seeing disk.
         kx, ky = np.meshgrid(np.arange(self.Npix) - float(self.Npix)/2.,np.arange(self.Npix) - float(self.Npix)/2.)
-        kk2 = (kx*kx + ky*ky)/(self.SeeingScale)**2
+        kk2 = (kx*kx + ky*ky)/(self.seeingScale)**2
         Pk = np.exp(-(kk2)**(5./6.)) #Kolmogorov power spectrum.
         Pr = np.real(np.fft.fft2(Pk))
         psf = np.fft.fftshift(Pr)
@@ -90,9 +90,9 @@ class Datacube(object):
     
     def makeDataCube(self):
         # draw the psf-convolved galaxy surface-brightness profile.
-        image_smeared = self.makePsfConvImage()
+        image = sim.makeImageBessel(self.galaxyModel)
         xx, yy = np.meshgrid(np.arange(self.Npix) - float(self.Npix)/2.,np.arange(self.Npix) - float(self.Npix)/2.)
-        vmap = sim.vmapModel(model,xx,yy)
+        vmap = sim.vmapModel(self.baseModel,xx,yy)
         self._loadTransparency()
         self._loadThroughput()
         self._loadSky()
@@ -101,20 +101,20 @@ class Datacube(object):
         # velocity,  and  interpolate  the  galaxy  spectrum  onto  the
         # redshift wavelength scale.
         c = 300000. # Speed of light, km/s
-        dataCube = np.zeros(self.Npix,self.Npix,self.nSpectralPixels)
+        dataCube = np.zeros([self.Npix,self.Npix,self.nSpectralPixels])
         thisSky = np.interp(self.lambdaObs,self.lambdaSky,self.skyFluxTemplate)
         thisTransparency = np.interp(self.lambdaObs,self.lambdaTransparency,self.transparencyTemplate)
-        for i in arange(self.Npix):
-            for j in arange(self.Npix):
+        for i in np.arange(self.Npix):
+            for j in np.arange(self.Npix):
                 z_velocity = vmap[i,j] / c 
-                lambdaRest = lambdaObs * (1 + self.z) * (1 + z_velocity)
-                dataCube[i,j,:] = np.interp(lambdaRest,self.lambdaGalaxy,self.GalaxyFlux) * \ 
-                  thisTransparency + thisSky
+                lambdaRest = self.lambdaObs * (1 + self.z) * (1 + z_velocity)
+                dataCube[i,j,:] = image[i,j] * (np.interp(lambdaRest,self.lambdaGalaxy,self.GalaxyFlux) * \
+                  thisTransparency + thisSky) / np.max(image)
         self.dataCube = dataCube
 
         # Finally, step through the datacube and convolve each spectral slice with the psf.
         psf = self._makePsfConvKernel()
-        for i in arange(self.nSpectralPixels):
+        for i in np.arange(self.nSpectralPixels):
             dataCube[:,:,i] = fftconvolve(dataCube[:,:,i],psf,mode="same")
 
         return dataCube
@@ -130,8 +130,8 @@ class Datacube(object):
         datadir = os.path.join(os.path.abspath(path),"../data/")
         skyFile = datadir+"kpno_sky.txt"
         skyTemplate = np.loadtxt(skyFile)
-        self.lambdaSky = sky[:,0]
-        self.skyFluxTemplate = 10.^((21.572-skyTemplate[:,1])/2.5)*1e-17
+        self.lambdaSky = skyTemplate[:,0]
+        self.skyFluxTemplate = 10.**((21.572-skyTemplate[:,1])/2.5)*1e-17
 
     def _loadTransparency(self):
         '''
@@ -141,9 +141,9 @@ class Datacube(object):
         path,filename = os.path.split(sim.__file__)
         datadir = os.path.join(os.path.abspath(path),"../data/")
         transparencyFile = datadir+"atmtrans_default.dat"
-        transparencyData = np.loadtxt(skyFile)
-        self.lambdaTransparency = transparency[:,0]
-        self.transparencyTemplate = transparency[:,1]
+        transparencyData = np.loadtxt(transparencyFile)
+        self.lambdaTransparency = transparencyData[:,0]
+        self.transparencyTemplate = transparencyData[:,1]
 
     def _loadThroughput(self):
         '''
@@ -161,7 +161,7 @@ class Datacube(object):
         '''
         # First, go and read in the continuum.
         path,filename = os.path.split(sim.__file__)
-        datadir = os.path.join(os.path.abspath(path),"../data/")
+        datadir = os.path.join(os.path.abspath(path),"../data/bc03_templates/")
         galaxyFile = datadir+templateFile
         galaxyData = np.loadtxt(galaxyFile)
         self.lambdaGalaxy = galaxyData[:,0]
@@ -175,17 +175,9 @@ class Datacube(object):
               np.exp(-( (self.lambdaGalaxy - line.Center)**2 )/(2 * line.Width))
 
 
+import matplotlib.pyplot as plt
 
-        
-
-
-
-            
-model = speclens.Model("B")
-#Draw an image.
-
-
-
-# Make a spectrum.
-
-# Interpolate the sky spectrum onto the wavelength range under consideration.
+data = Datacube()
+cube = data.makeDataCube()
+plot = plt.plot(cube[50,50,:])
+plt.savefig("cube_peak_spectrum")
