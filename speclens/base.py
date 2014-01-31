5cd70d292d23fce487b10bdfd4a1e02b1461e2a2
Matt George <mattgeorge1@gmail.com>
2014-01-30 18:44:24 -0800

    improve class definitions in base.py to work with sim functions
    add vSampConvolve attr to Detector class
    
    make vSampShape a property of Detector class with only a getter, since
    it depends (uniquely) on vSampConfig and we want it determined on the
    fly.
    
    add PSF class to hold atmosFWHM (also start toward adding complexity
    for #18)
    
    replace atmosFWHM in Observable class with PSF instance
    
    assign vSampPA attr to detector instance within Observable
    
    add convOpt to Model class (pixel, galsim, or None). not sure where
    else to hold this.
    
    remove old Galaxy, Observation, and Model classes, which were replaced
    in a previous commit

--- speclens/base.py
+++ speclens/base.py
@@ -12,8 +12,26 @@ class Detector(object):
         self.vSampConfig="crossslit"
         self.vSampSize=0.5  # arcseconds (radius for fibers, side length for pixels)
         self.nVSamp=20
+        self.vSampConvolve=True
+
+    @property
+    def vSampShape(self):
+        # see sim.getSamplePos for acceptable vSampConfig values
+        shapes={"hex":"circle",
+                "hexNoCen":"circle",
+                "triNoCen":"circle",
+                "slit":"square",
+                "crossslit":"square",
+                "ifu":"square"
+                }
+        return shapes[self.vSampConfig]
 
 
+class PSF(object):
+    def __init__(self):
+        self.atmosFWHM=1.
+        self.atmosFWHMErr=0.
+        
 class Observable(object):
     """Collection of data and self-description for lnP calculation
 
@@ -47,8 +65,6 @@ class Observable(object):
         self.bulgeNuErr=0
         self.galFlux=1.
         self.galFluxErr=0.
-        self.atmosFWHM=1.
-        self.atmosFWHMErr=0.
 
         # parameters derived from raw data, with errors
         #   (typically free in the model)
@@ -64,9 +80,9 @@ class Observable(object):
         self.imageErr=None
         self.datacubeErr=None
 
-        # parameters describing detector
+        # parameters describing detector and seeing
         self.detector=Detector()
-
+        self.psf=PSF()
 
     def setAttr(self, **kwargs):
         """Set/update arbitrary attribute list with **kwargs"""
@@ -89,9 +105,9 @@ class Observable(object):
             self.xObs=xObs
             self.yObs=yObs
         else:
-            self.vSampPA=vSampPA
-            pos,self.vSampShape = speclens.sim.getSamplePos(self.detector.nVSamp,
-                self.detector.vSampSize, self.detector.vSampConfig, sampPA=self.vSampPA)
+            self.detector.vSampPA=vSampPA
+            pos = sim.getSamplePos(self.detector.nVSamp,
+                self.detector.vSampSize, self.detector.vSampConfig, sampPA=vSampPA)
             self.xObs, self.yObs = pos
 
         self.xObsErr=xObsErr
@@ -173,7 +189,7 @@ class Galaxy(object):
     # These are accessed with the normal syntax (e.g. model.diskBA)
     @property
     def diskBA(self):
-        return speclens.sim.convertInclination(diskCA=self.diskCA,
+        return sim.convertInclination(diskCA=self.diskCA,
             inc=np.arccos(self.cosi))
 
     @property
@@ -196,7 +212,8 @@ class Model(object):
     def __init__(self):
         self.obs=Observable()
         self.source=Galaxy()
-
+        self.convOpt="pixel"
+        
     def defineModelPars(self, modelName):
         self.modelName=modelName
         if(modelName=="A"):
@@ -276,191 +293,3 @@ class Model(object):
             self.source.g2 = g2
         else:
             raise ValueError(self.modelName)
-
-class Galaxy(object):
-    """Galaxy class defining intrinsic parameters of a galaxy object"""
-
-    def __init__(self, diskRadius=1., diskVRadius=2.2, diskCA=0.2,
-                 diskSersic=1., diskVCirc=200., bulgeFraction=0.,
-                 bulgeRadius=1., bulgeSersic=4., galFlux=1.):
-        self.diskRadius=diskRadius
-        self.diskVRadius=diskVRadius
-        self.diskCA=diskCA
-        self.diskSersic=diskSersic
-        self.diskVCirc=diskVCirc
-        self.bulgeFraction=bulgeFraction
-        self.bulgeRadius=bulgeRadius
-        self.bulgeSersic=bulgeSersic
-        self.galFlux=galFlux
-
-class Observation(object):
-    """Observation class takes a galaxy and produces observables"""
-
-    def __init__(self, galaxy, redshift=0.5, diskPA=0., diskBA=0.7,
-                 g1=0., g2=0., atmosFWHM=None, pixScale=0.1, nPix=100,
-                 obsConfig="crossSlit", obsSize=1., nObs=10):
-        self.galaxy=galaxy
-        self.redshift=redshift
-        self.diskPA=diskPA
-        self.diskBA=diskBA
-        self.g1=g1
-        self.g2=g2
-        self.atmosFWHM=atmosFWHM
-        self.pixScale=pixScale
-        self.nPix=nPix
-        self.obsConfig=obsConfig
-        self.obsSize=obsSize
-        self.nObs=nObs
-
-    def _drawMaps(self):
-        vmapArr,fluxVMapArr,imgArr=sim.makeGalVMap2(self.galaxy.bulgeSersic,
-            self.galaxy.bulgeRadius, self.galaxy.diskSersic,
-            self.galaxy.diskRadius, self.galaxy.bulgeFraction,
-            np.cos(np.deg2rad(self.diskInclination)), self.diskPA,
-            self.galaxy.galFlux, "flat", [self.galaxy.diskVCirc],
-            self.g1, self.g2)
-        self.vmap=vmapArr
-        self.fvmap=fluxVMapArr
-        self.image=imgArr
-    
-    def getImage(self):
-        try:
-            return self.image
-        except AttributeError:
-            self._drawMaps()
-            return self.image
-
-    def getVMap(self):
-        try:
-            return self.vmap
-        except AttributeError:
-            self._drawMaps()
-            return self.vmap
-
-    def getFVMap(self):
-        try:
-            return self.fvmap
-        except AttributeError:
-            self._drawMaps()
-            return self.fvmap
-
-    def getVelocities(self):
-        pass
-
-    def addNoise(self, sigma):
-        pass
-
-
-class Model(object):
-    """Model class defining fit parameters
-
-    Define all galaxy and observational parameters and describe
-    which ones will be fit or fixed.
-    """
-
-    def __init__(self, modelName, galName="default"):
-        self.modelName=modelName
-        self._setupAttr(galName)
-
-        # Define priors and guess for each type of model
-        # Note: priors are those used for fitting, 
-        #       inputPriors are the distributions for generating ensembles.
-        if(modelName=="A"):
-            self.description="""Fixed disk thickness, flat rotation curve
-
-                diskPA - disk position angle in degrees [0,180)
-                cosi - cosine of the disk inclination (0=edge on, 1=face on)
-                vmax - circular velocity (>0)
-                g1 - shear 1 (abs<0.5)
-                g2 - shear 2 (abs<0.5)
-            """
-            self.origPars=[self.diskPA, self.cosi, np.log10(self.vCirc), self.g1, self.g2]
-            self.labels=np.array(["PA","cos(i)","lg10(vc)","g1","g2"])
-            self.origGuess=np.array([10.,0.5,np.log10(200.),0.,0.])
-            self.origGuessScale=np.array([30.,0.2,0.06,0.02,0.02])
-            self.origPriors=[("wrap",0.,180.), ("uniform",0.01,0.99),
-                             ("norm",np.log10(200.),0.06),
-                             ("uniform",-0.5,0.5),
-                             ("uniform",-0.5,0.5)]
-            self.inputPriors=[("uniform",0.0,180.),
-                             ("uniform",0.01,0.99), ("fixed",np.log10(200.)),
-                             ("norm",0.,0.05), ("norm",0.,0.05)]
-
-        elif(modelName=="B"):
-            self.description="""Free disk thickness, flat rotation curve
-
-                diskPA - disk position angle in degrees [0,180)
-                cosi - cosine of the disk inclination (0=edge on, 1=face on)
-                diskCA - edge on disk thickness ratio (0=thin,1=sphere)
-                log10(vmax) - circular velocity
-                g1 - shear 1 (abs<0.5)
-                g2 - shear 2 (abs<0.5)
-            """
-            self.origPars=[self.diskPA, self.cosi, self.diskCA,
-                           np.log10(self.vCirc), self.g1, self.g2]
-            self.labels=np.array(["PA","cos(i)","c/a","lg10(vc)","g1","g2"])
-            self.origGuess=np.array([10.,0.5,0.2,np.log10(200.),0.,0.])
-            self.origGuessScale=np.array([30.,0.2,0.1,0.06,0.02,0.02])
-            self.origPriors=[("wrap",0.,180.), ("uniform",0.01,0.99),
-                             ("truncnorm",0.2,0.05,0.,1.),
-                             ("norm",np.log10(200.),0.06),
-                             ("uniform",-0.5,0.5),
-                             ("uniform",-0.5,0.5)]
-            self.inputPriors=[("uniform",0.0,180.),
-                             ("uniform",0.01,0.99), ("fixed",0.2),
-                             ("fixed",np.log10(200.)),
-                             ("norm",0.,0.05), ("norm",0.,0.05)]
-
-        else:
-            raise ValueError(modelName)
-
-    def updatePars(self, pars):
-        """Take a set of model pars and update stored values
-
-        Since fit.lnProbVMapModel requires a pars array and other
-        functions require a model object, this function takes a given pars
-        array and reassigns the stored values in the model object.
-        """
-        if(self.modelName=="A"):
-            self.diskPA, self.cosi, log10vCirc, self.g1, self.g2 = pars
-            self.diskBA = sim.convertInclination(diskCA=self.diskCA, inc=np.arccos(self.cosi))
-            self.vCirc = 10.**log10vCirc
-        elif(self.modelName=="B"):
-            self.diskPA, self.cosi, self.diskCA, log10vCirc, self.g1, self.g2 = pars
-            self.diskBA = sim.convertInclination(diskCA=self.diskCA, inc=np.arccos(self.cosi))
-            self.vCirc = 10.**log10vCirc
-        else:
-            raise ValueError(self.modelName)
-
-    def _setupAttr(self, galName="default"):
-        """Define Model attributes"""
-
-        if(galName == "default"):
-            self.diskRadius=1.
-            self.diskVRadius=2.2
-            self.diskCA=0.2
-            self.diskNu=0.5
-            self.bulgeFraction=0.
-            self.bulgeRadius=1.
-            self.bulgeNu=-0.6
-            self.galFlux=1.
-            self.rotCurveOpt="arctan"
-            self.vCirc=200.
-            self.rotCurvePars=[self.vCirc,self.diskVRadius]
-            self.redshift=0.5
-            self.diskPA=0.
-            self.cosi=np.cos(np.deg2rad(30.))
-            self.diskBA=sim.convertInclination(diskCA=self.diskCA,inc=np.arccos(self.cosi))
-            self.g1=0.
-            self.g2=0.
-            self.atmosFWHM=1.
-            self.pixScale=0.1  # arcseconds per pixel
-            self.nPix=100
-            self.vSampConfig="crossslit"
-            self.vSampSize=0.5  # arcseconds (radius for fibers, side length for pixels)
-            self.nVSamp=20
-            self.vSampPA=self.diskPA
-            self.vSampConvolve=True
-            self.convOpt="pixel"
-        else:
-            raise ValueError(galName)
