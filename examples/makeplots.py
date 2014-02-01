5c24939e50ccb2e4d444fd0c9b691e6cdff7ef85
Matt George <mattgeorge1@gmail.com>
2014-01-31 16:03:17 -0800

    updating makeplots examples with new objects (not finished)

--- examples/makeplots.py
+++ examples/makeplots.py
@@ -15,8 +15,8 @@ except ImportError: # add parent dir to python search path
     
 # This is a driver for functions in sim.py to create some figures for a proposal
 
-def getShapes(model):
-    ell=speclens.sim.defineEllipse(model)
+def getShapes(galaxy):
+    ell=speclens.sim.defineEllipse(galaxy)
     lines=speclens.sim.getEllipseAxes(ell)
     return (ell,lines)
 
@@ -36,51 +36,49 @@ def shearVMapPlot(plotDir, figExt="pdf", showPlot=False):
     trim=2.5
 
     # Start with basic model
-    model=speclens.Model("A",galName="default")
-    model.diskCA=0.
-    model.rotCurveOpt="flat"
-    model.rotCurvePars=[model.vCirc]
-    model.cosi=0.75
-    model.diskBA=speclens.sim.convertInclination(inc=np.arccos(model.cosi),diskCA=model.diskCA)
-    
-    ell,lines=getShapes(model)
-    vmap,fluxVMap,thinImg,img = speclens.sim.makeGalVMap2(model)
+    galaxy = speclens.Galaxy()
+    galaxy.diskCA=0.
+    galaxy.rotCurveOpt="flat"
+    galaxy.cosi=0.75
+
+    detector = speclens.Detector()
+
+    ell,lines=getShapes(galaxy)
+    vmap,fluxVMap,thinImg,img = speclens.sim.makeGalVMap2(galaxy, detector)
 
     plt.clf()
-    speclens.plot.showImage(vmap, model, None, None,
+    speclens.plot.showImage(vmap, detector, None, None,
         filename="{}/fig1a.{}".format(plotDir,figExt), trim=trim,
         ellipse=ell, lines=lines, lw=lw, 
         title=r"cos$(i)=0.75$, $\gamma_+=0, \gamma_{\times}=0$",
         showPlot=showPlot)
 
     # Now change the inclination
-    model.cosi=0.5
-    model.diskBA=speclens.sim.convertInclination(inc=np.arccos(model.cosi),diskCA=model.diskCA)
+    galaxy.cosi=0.5
 
-    ellInc,linesInc=getShapes(model)
-    vmapInc,fluxVMapInc,thinImgInc,imgInc = speclens.sim.makeGalVMap2(model)
+    ellInc,linesInc=getShapes(galaxy)
+    vmapInc,fluxVMapInc,thinImgInc,imgInc = speclens.sim.makeGalVMap2(galaxy, detector)
 
     plt.clf()
-    speclens.plot.showImage(vmapInc, model, None, None,
+    speclens.plot.showImage(vmapInc, detector, None, None,
         filename="{}/fig1b.{}".format(plotDir,figExt), trim=trim,
         ellipse=ellInc, lines=linesInc, lw=lw, 
         title=r"cos$(i)=0.5$, $\gamma_+=0, \gamma_{\times}=0$",
         showPlot=showPlot)
     
     # Now undo inclination and apply a shear that mimics it
-    model.cosi=0.75
-    model.diskBA=speclens.sim.convertInclination(inc=np.arccos(model.cosi),diskCA=model.diskCA)
-    model.g1=0.2
+    galaxy.cosi=0.75
+    galaxy.g1=0.2
 
-    ellG1,linesG1=getShapes(model) # note these are the unsheared shapes
-    ellSheared=speclens.sim.shearEllipse(ellG1,model.g1,model.g2)
-    linesSheared=speclens.sim.shearLines(linesG1,model.g1,model.g2)
+    ellG1,linesG1=getShapes(galaxy) # note these are the unsheared shapes
+    ellSheared=speclens.sim.shearEllipse(ellG1,galaxy.g1,galaxy.g2)
+    linesSheared=speclens.sim.shearLines(linesG1,galaxy.g1,galaxy.g2)
     linesObs=speclens.sim.getEllipseAxes(ellSheared)
 
-    vmapG1,fluxVMapG1,thinImgG1,imgG1 = speclens.sim.makeGalVMap2(model)
+    vmapG1,fluxVMapG1,thinImgG1,imgG1 = speclens.sim.makeGalVMap2(galaxy, detector)
 
     plt.clf()
-    speclens.plot.showImage(vmapG1, model, None, None,
+    speclens.plot.showImage(vmapG1, detector, None, None,
         filename="{}/fig1c.{}".format(plotDir,figExt), trim=trim,
         ellipse=ellSheared, lines=linesObs, lw=lw, 
         title=r"cos$(i)=0.75$, $\gamma_+=0.2, \gamma_{\times}=0$",
@@ -88,19 +86,19 @@ def shearVMapPlot(plotDir, figExt="pdf", showPlot=False):
     
 
     # Finally show the cross shear
-    model.g1=0.
-    model.g2=0.2
+    galaxy.g1=0.
+    galaxy.g2=0.2
     
-    ellG2,linesG2=getShapes(model) # note these are the unsheared shapes
-    ellSheared=speclens.sim.shearEllipse(ellG2,model.g1,model.g2)
-    linesSheared=speclens.sim.shearLines(linesG2,model.g1,model.g2)
+    ellG2,linesG2=getShapes(galaxy) # note these are the unsheared shapes
+    ellSheared=speclens.sim.shearEllipse(ellG2,galaxy.g1,galaxy.g2)
+    linesSheared=speclens.sim.shearLines(linesG2,galaxy.g1,galaxy.g2)
     linesObs=speclens.sim.getEllipseAxes(ellSheared)
     
-    vmapG2,fluxVMapG2,thinImgG2,imgG2 = speclens.sim.makeGalVMap2(model)
+    vmapG2,fluxVMapG2,thinImgG2,imgG2 = speclens.sim.makeGalVMap2(galaxy, detector)
 
     plt.clf()
     
-    speclens.plot.showImage(vmapG2, model, None, None,
+    speclens.plot.showImage(vmapG2, detector, None, None,
         filename="{}/fig1d.{}".format(plotDir,figExt), trim=trim,
         ellipse=ellSheared,
         lines=np.array([linesSheared,linesObs]).reshape(4,4),
@@ -116,31 +114,32 @@ def samplingPlot(plotDir, figExt="pdf", showPlot=False):
 
     Illustrate spatial sampling of the velocity field
     including the effects of seeing. Uses default sampling
-    configuration from Model class, can be changed for
+    configuration from Detector class, can be changed for
     fibers/slits/ifu etc.
     """
 
     trim=1.
 
-    model=speclens.Model("A")
-    model.rotCurveOpt="flat"
-    model.rotCurvePars=np.array([model.vCirc])
+    galaxy = speclens.Galaxy()
+    galaxy.rotCurveOpt="flat"
 
-    vmap,fluxVMap,thinImg,img = speclens.sim.makeGalVMap2(model)
+    detector = speclens.Detector()
+    detector.vSampPA = galaxy.diskPA
+    
+    vmap,fluxVMap,thinImg,img = speclens.sim.makeGalVMap2(galaxy, detector)
 
-    pos,sampShape=speclens.sim.getSamplePos(model.nVSamp,
-        model.vSampSize, model.vSampConfig, sampPA=model.vSampPA)
+    pos=speclens.sim.getSamplePos(detector.nVSamp,
+        detector.vSampSize, detector.vSampConfig, sampPA=detector.vSampPA)
 
     xpos,ypos=pos
-    model.vSampShape=sampShape
 
     plt.clf()
-    speclens.plot.showImage(img, model, xpos, ypos,
+    speclens.plot.showImage(img, detector, xpos, ypos,
         filename="{}/fig2a".format(plotDir,figExt), trim=trim,
         colorbar=True, cmap=matplotlib.cm.gray, colorbarLabel=None,
         showPlot=showPlot)
     plt.clf()
-    speclens.plot.showImage(fluxVMap, model, xpos, ypos,
+    speclens.plot.showImage(fluxVMap, detector, xpos, ypos,
         filename="{}/fig2b".format(plotDir,figExt), trim=trim,
         colorbar=True, cmap=matplotlib.cm.jet, colorbarLabel=None,
         showPlot=showPlot)
@@ -157,76 +156,78 @@ def vThetaPlot(plotDir, figExt="pdf", showPlot=False):
     """
 
     plt.clf()
+
+    # Define galaxy and detector
+    galaxy = speclens.Galaxy()
+    galaxy.rotCurveOpt="flat"
+    galaxy.cosi=0.8
+
+    detector = speclens.Detector()
+    detector.vSampConfig="hexNoCen"
+    detector.nVSamp=6
+    detector.vSampSize=1.
+    detector.vSampConvolve=False
+    detector.vSampPA=galaxy.diskPA
     
-    # Define galaxy model    
-    model=speclens.Model("A")
-    model.vSampConfig="hexNoCen"
-    model.rotCurveOpt="flat"
-    model.rotCurvePars=[model.vCirc]
-    model.cosi=0.8
-    model.nVSamp=6
-    model.vSampSize=1.
-    model.atmosFWHM=None
-    model.vSampConvolve=False
+    psf = speclens.PSF()
+    psf.atmosFWHM=None
 
     # Get velocity sampling positions
-    pos,sampShape=speclens.sim.getSamplePos(model.nVSamp,model.vSampSize,model.vSampConfig)
+    pos=speclens.sim.getSamplePos(detector.nVSamp, detector.vSampSize,
+        detector.vSampConfig, sampPA=detector.vSampPA)
     sigma=30. # velocity unc in km/s
     xpos,ypos=pos
-    model.vSampShape=sampShape
     
     # Evaluate model as smooth function of azimuthal angle
     theta=np.linspace(0,2.*np.pi,num=200)
-    xvals=2.*model.vSampSize*np.cos(theta)
-    yvals=2.*model.vSampSize*np.sin(theta)
-    vvals=speclens.sim.vmapModel(model, xvals, yvals)
+    xvals=2.*detector.vSampSize*np.cos(theta)
+    yvals=2.*detector.vSampSize*np.sin(theta)
+    vvals=speclens.sim.vmapModel(galaxy, xvals, yvals)
 
     plt.plot(np.rad2deg(theta), vvals, color="blue", linestyle='-',
              lw=3, label="Fiducial: PA={}, cos(i)={:0.2}".format(
-                model.diskPA, model.cosi) + 
+                galaxy.diskPA, galaxy.cosi) + 
              r", v$_{max}$"+"={}, g1={}, g2={}".format(
-                 model.vCirc, model.g1, model.g2))
+                 galaxy.vCirc, galaxy.g1, galaxy.g2))
 
     thetasamp=np.linspace(0,2.*np.pi,num=6,endpoint=False)
-    xsamp=2.*model.vSampSize*np.cos(thetasamp)
-    ysamp=2.*model.vSampSize*np.sin(thetasamp)
-    vsamp=speclens.sim.vmapModel(model, xsamp, ysamp)
+    xsamp=2.*detector.vSampSize*np.cos(thetasamp)
+    ysamp=2.*detector.vSampSize*np.sin(thetasamp)
+    vsamp=speclens.sim.vmapModel(galaxy, xsamp, ysamp)
     verr=np.repeat(sigma,xsamp.size)
-    
+
     plt.errorbar(np.rad2deg(thetasamp), vsamp, yerr=verr, fmt=None,
         lw=2, ecolor='black', elinewidth=5, capsize=7)
-    
-    model.cosi+=0.2
-    vvals=speclens.sim.vmapModel(model, xvals, yvals)
+
+    galaxy.cosi+=0.2
+    vvals=speclens.sim.vmapModel(galaxy, xvals, yvals)
     plt.plot(np.rad2deg(theta), vvals, color="green", linestyle="--",
-        lw=2, label="cos(i)={:0.2}".format(model.cosi))
-    
-    model.cosi-=0.2
-    model.diskPA+=20.
-    vvals=speclens.sim.vmapModel(model, xvals, yvals)
+        lw=2, label="cos(i)={:0.2}".format(galaxy.cosi))
+
+    galaxy.cosi-=0.2
+    galaxy.diskPA+=20.
+    vvals=speclens.sim.vmapModel(galaxy, xvals, yvals)
     plt.plot(np.rad2deg(theta), vvals, color="orange", linestyle="-.",
-        lw=2, label="PA={}".format(model.diskPA))
-    
-    model.diskPA-=20.
-    model.vCirc+=50.
-    model.rotCurvPars=[model.vCirc]
-    vvals=speclens.sim.vmapModel(model, xvals, yvals)
+        lw=2, label="PA={}".format(galaxy.diskPA))
+
+    galaxy.diskPA-=20.
+    galaxy.vCirc+=50.
+    vvals=speclens.sim.vmapModel(galaxy, xvals, yvals)
     plt.plot(np.rad2deg(theta), vvals, color="red", linestyle=":",
-        lw=2, label=r"v$_{max}$"+"={}".format(model.vCirc))
-    
-    model.vCirc-=50.
-    model.rotCurvPars=[model.vCirc]
-    model.g1+=0.1
-    vvals=speclens.sim.vmapModel(model, xvals, yvals)
+        lw=2, label=r"v$_{max}$"+"={}".format(galaxy.vCirc))
+
+    galaxy.vCirc-=50.
+    galaxy.g1+=0.1
+    vvals=speclens.sim.vmapModel(galaxy, xvals, yvals)
     plt.plot(np.rad2deg(theta), vvals, color="yellow", linestyle="-",
-        lw=2, label="g1"+"={}".format(model.g1))
+        lw=2, label="g1"+"={}".format(galaxy.g1))
     
-    model.g1-=0.1
-    model.g2+=0.1
-    vvals=speclens.sim.vmapModel(model, xvals, yvals)
+    galaxy.g1-=0.1
+    galaxy.g2+=0.1
+    vvals=speclens.sim.vmapModel(galaxy, xvals, yvals)
 
     plt.plot(np.rad2deg(theta), vvals, color="magenta",
-        linestyle="--", lw=2, label="g2"+"={}".format(model.g2))
+        linestyle="--", lw=2, label="g2"+"={}".format(galaxy.g2))
     
     plt.legend(loc="upper right",prop={'size':14},frameon=False)
     
@@ -234,7 +235,7 @@ def vThetaPlot(plotDir, figExt="pdf", showPlot=False):
     plt.ylabel('v(r$_{proj}$=2\") (km/s)')
     plt.xlim((-5,365))
     plt.ylim(np.array([-250,250]))
-    
+
     plt.gcf().subplots_adjust(left=0.15)
     plt.savefig("{}/fig3.{}".format(plotDir,figExt))
     if(showPlot):
@@ -251,18 +252,21 @@ def modelConstraintPlot(chainDir, plotDir, figExt="pdf", showPlot=False):
     fit likelihood. Plot joint posterior constraints and store chain.
     """
 
-    nThreads=8
-    
-    model=speclens.Model("A")
+    nThreads = 8
 
-    sigma=10.
-    ellErr=np.array([10.,0.1])
+    model = speclens.Model("A")
+
+    observation = speclens.Observable()
+    observation.vObsErr = np.repeat(10., observation.detector.nVSamp)
+    observation.diskPAErr = 10.
+    observation.diskBAErr = 0.1
+    observation.setPointing(vSampPA = observation.diskPA)
 
     # first try w/o PSF and fiber convolution
     galID=0
-    model.atmosFWHM=None
-    model.vSampConvolve=False
     model.convOpt=None
+    observation.psf.atmosFWHM=None
+    observation.detector.vSampConvolve=False
 
     xvals,yvals,vvals,ellObs,inputPars = speclens.ensemble.makeObs(
         model, sigma=sigma, ellErr=ellErr, randomPars=False)
@@ -273,12 +277,12 @@ def modelConstraintPlot(chainDir, plotDir, figExt="pdf", showPlot=False):
         sigma, ellObs, ellErr, model, figExt=figExt, addNoise=True,
         nThreads=nThreads, seed=0)
 
-
     # now try with PSF and fiber convolution
     galID=1
-    model.atmosFWHM=1.
-    model.vSampConvolve=True
     model.convOpt="pixel"
+    observation.psf.atmosFWHM=1.
+    observation.detector.vSampConvolve=True
+    observation.makeConvolutionKernel(model.convOpt)
 
     xvals,yvals,vvals,ellObs,inputPars = speclens.ensemble.makeObs(
         model, sigma=sigma, ellErr=ellErr, randomPars=False)
